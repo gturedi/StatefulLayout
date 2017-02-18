169c18a48f0059bace361dd6b7789df6cc36f04d
gturedi <turedi.gokhan@gmail.com>
2017-02-18 20:29:29 +0300

    formatted code

--- app/src/main/java/com/gturedi/app/MainActivity.java
+++ app/src/main/java/com/gturedi/app/MainActivity.java
@@ -35,31 +35,46 @@ public class MainActivity
     }
 
     public void loading(View view) {
-        stateful.showLoading();
+        //stateful.showLoading();
+        //stateful.showLoading(R.string.testMessage);
+        stateful.showLoading(getString(R.string.testMessage));
     }
 
     public void empty(View view) {
-        stateful.showEmpty();
+        //stateful.showEmpty();
+        //stateful.showEmpty(R.string.testMessage);
+        stateful.showEmpty(getString(R.string.testMessage));
     }
 
     public void error(View view) {
-        stateful.showError(runnable);
+        //stateful.showError(runnable);
+        //stateful.showError(R.string.testMessage, runnable);
+        stateful.showError(getString(R.string.testMessage), runnable);
     }
 
     public void offline(View view) {
-        stateful.showOffline(runnable);
+        //stateful.showOffline(runnable);
+        //stateful.showOffline(R.string.testMessage, runnable);
+        stateful.showOffline(getString(R.string.testMessage), runnable);
     }
 
     public void locationOff(View view) {
-        stateful.showLocationOff(runnable);
+        //stateful.showLocationOff(runnable);
+        //stateful.showLocationOff(R.string.testMessage, runnable);
+        stateful.showLocationOff(getString(R.string.testMessage), runnable);
     }
 
     public void custom(View view) {
+        //stateful.showCustom(new CustomStateOptions());
         //stateful.showCustom(new CustomStateOptions().image(R.drawable.ic_bluetooth_disabled_black_24dp));
         //stateful.showCustom(new CustomStateOptions().image(R.drawable.ic_bluetooth_disabled_black_24dp).message("please open bluetooth"));
         //stateful.showCustom(new CustomStateOptions().message("hey yow!"));
         //stateful.showCustom(new CustomStateOptions().message("hey yow!").buttonAction(runnable));
-        stateful.showCustom(new CustomStateOptions().image(R.drawable.ic_bluetooth_disabled_black_24dp).message("please open bluetooth").buttonText("settings").buttonAction(runnable));
+        stateful.showCustom(new CustomStateOptions()
+                .image(R.drawable.ic_bluetooth_disabled_black_24dp)
+                .message("please open bluetooth")
+                .buttonText("settings")
+                .buttonAction(runnable));
     }
 
 }
